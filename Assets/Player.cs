bce7419122d45419e35af3fb36732f94efe39d5a
Linkcube <poohbah125@gmail.com>
2015-01-24 03:05:44 -0800

    Cooldown timers and locks.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,31 +2,50 @@ using UnityEngine;
 using System.Collections;
 
 public abstract class Player : MonoBehaviour {
-
+	
 	public CharacterControllerThirdPerson body;
-
+	
 	public float health;
 	public int points;
 	public float coolDownTimeOne;
 	public float coolDownTimeTwo;
-
+	
 	private float coolDownOneTimer;
 	private float coolDownTwoTimer;
-
+	
 	public void dealDamage(float damage) {
 		this.health = this.health - damage;
 		if (this.health <= 0) {
 			//game over		
 		}
 	}
+	
+	public virtual void doSpecialOne() {
+		if (coolDownOneTimer >= coolDownTimeOne) {
+			// do the action	
+			coolDownOneTimer = 0;
+		}
+	} 
+	public virtual void doSpecialTwo() {
+		if (coolDownTwoTimer >= coolDownTimeTwo) {
+			// do the action
+			coolDownTwoTimer = 0;
+		}
+	} 
+	
 
-	public virtual void doSpecialOne() {} 
-	public virtual void doSpecialTwo() {} 
-
-
-	void Update() {
-
+	public void Update() {
+		updateTimers();
 	}
-
-
+	
+	private void updateTimers() {
+		if (coolDownOneTimer < coolDownTimeOne){
+			coolDownOneTimer += Time.deltaTime;
+		}
+		if (coolDownTwoTimer < coolDownTimeTwo){
+			coolDownTwoTimer += Time.deltaTime;
+		}
+	}
+	
+	
 }
